7e58525f33ea86e0c689b52cea67b57af2424d9c
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-06-01 16:40:19 -0300

    Fist Steps

---
+++ FirstSteps/02.js
@@ -0,0 +1,23 @@
+//bloco 1
+//{
+//    console.log(1)
+//    console.log(3)
+//    console.log(5)
+//    console.log(7)
+//    console.log(9)
+//}
+//
+//bloco 2 
+//{
+//    console.log(2)
+//    console.log(4)
+//    console.log(6)
+//    console.log(8)
+//    console.log(10)
+//}
+
+const count = 10 // definir uma constante, não é possível alterar 
+let nome = 'Caneta' // definir uma variável
+let preço = 1.99 
+
+console.log("Nome: "+ nome +"\nPreço: " + preço + "\nQuantidade: " + count)
---
+++ FirstSteps/03.js
@@ -0,0 +1,6 @@
+let raio = 10
+const PI = 3.14 // ou Math.PI
+
+let circunferencia = raio * raio * PI
+
+console.log('Circunferencia: ' + circunferencia +'m²')
