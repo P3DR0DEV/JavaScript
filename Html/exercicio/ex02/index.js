1a2f6e6f84444021657108c029e597d5c651d6b7
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-06-30 17:39:18 -0300

    Ex

---
+++ Html/exercicio/ex02/index.js
@@ -0,0 +1,13 @@
+let num = prompt('Digite um número: ') // recebe o número digitado em forma de string
+num = Number(num) // transforma a string em número
+const numTitle = document.getElementById('num-title') 
+const texto = document.getElementById('texto')
+
+numTitle.innerHTML = num
+
+texto.innerHTML += `<p>Seu número menos 2 é: ${num - 2}</p>`
+texto.innerHTML += `<p>A raiz quadrada de ${num} é: ${num ** 1/2}</p>`
+texto.innerHTML += `<p>É NaN: ${num.isNaN()}</p>`
+texto.innerHTML += `<p>Arredondando ${num} para baixo é: ${Math.floor(num)} </p>`
+texto.innerHTML += `<p>Arredondando ${num} para cima é: ${Math.ceil(num)} </p>`
+texto.innerHTML += `<p>Com duas casas decimais: ${num.toFixed(2)}</p>`
