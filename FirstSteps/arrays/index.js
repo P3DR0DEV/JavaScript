ad4717558d15361e733047358f97a40b5877efee
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-01 15:32:52 -0300

    Array

---
+++ FirstSteps/arrays/index.js
@@ -0,0 +1,22 @@
+let alunos = ['Felipe', 'Helen', 'Pedrão'] //array
+console.log(alunos)
+
+alunos.push('Pedro') //adiciona um elemento ao final do array
+console.log(alunos)
+
+alunos.unshift('Pablo') // adiciona um elemento no inicio do array
+console.log(alunos) 
+
+alunos.unshift('')
+console.log(alunos)
+
+alunos[0] = 'Lucelia' //troca o elemento indicado entre []
+console.log(alunos)
+
+alunos.pop() //remove o ultimo elemento
+console.log(alunos)
+
+alunos.shift() // remove o primeiro elemento 
+console.log
+
+
