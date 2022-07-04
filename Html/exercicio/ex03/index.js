daba7de0cbbe6cd0ac0b65b2530c2b3e9fbd54f6
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-04 12:11:20 -0300

    eX 03

---
+++ Html/exercicio/ex03/index.js
@@ -0,0 +1,31 @@
+function escopoGlobal(){
+    const form = document.querySelector('.form')
+    // form.onsubmit = function(evento){
+    //     evento.preventDefault()
+    // };
+
+    const pessoas = []
+    function recebeEventoForm(evento){
+        evento.preventDefault()
+        const nome = form.querySelector('.nome')
+        const sobrenome = form. querySelector('.sobrenome')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoas.push({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+        console.log(pessoas)
+        
+
+        const addArray = document.querySelector('.resultado')
+        addArray.innerHTML += `<p>${nome.value} ${sobrenome.value}, ${peso.value}, ${altura.value}</p>`
+    }
+
+    form.addEventListener('submit', recebeEventoForm)
+}
+
+escopoGlobal()
