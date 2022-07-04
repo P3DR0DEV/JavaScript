586dac7783bcb2377de462bf1da597468f0ed99a
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-04 17:32:04 -0300

    Condition

---
+++ FirstSteps/If, elseif, else/index.js
@@ -0,0 +1,9 @@
+const hora = 15
+
+if (hora <= 11 && hora >= 0){
+    console.log('Bom dia')
+} else if (hora >= 12 && hora < 18){
+    console.log('Boa tarde')
+} else{
+    console.log('Boa noite')
+}
