519c67541cb9ab7b6274af1e560f001ec2957690
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-04 10:46:56 -0300

    Copy test

---
+++ FirstSteps/arrays/copy.js
@@ -0,0 +1,4 @@
+let a = [1, 2, 3]
+let b = a // b referencia a, o que eu alterar no a também altera no b
+
+let c =[...a] // copia os valores do a, independentemente, ou seja, se alterar o a, não modifica o c 
