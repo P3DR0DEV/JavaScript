7dcd70e9f2ddedca57c04a3479a21347cd64edf9
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-06 17:37:05 -0300

    Import

---
+++ FirstSteps/OperaçãoTernária.js
@@ -0,0 +1,5 @@
+const pontuaçãoUsuário = 1000 
+
+const nivelUsuario = pontuaçãoUsuário >= 1000 ? 'Usuário VIP': 'Usuário Normal'
+//                                              Verdadeiro      Falso
+//condição ? Verdadeiro: Falso
