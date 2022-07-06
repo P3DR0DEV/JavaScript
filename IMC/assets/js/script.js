377213bfb88694a6c2ae9d82ca29c85cacd4a9d4
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-06 12:36:26 -0300

    IMC add

--- IMC/assets/js/script.js
+++ IMC/assets/js/script.js
@@ -1,5 +1,6 @@
 const form = document.querySelector('.calculadora')
 
+// executa o código quando o botão for acionado 
 form.addEventListener('submit', function(e){
     e.preventDefault()
     const inputPeso = e.target.querySelector('#peso')
@@ -10,56 +11,58 @@ form.addEventListener('submit', function(e){
 
     if (!peso) {
         setResultado('Peso inválido')
-        return
+        return 
     } 
     if (!altura) {
-        setResultado('Altura inválido')
-        return
+        setResultado('Altura inválida')
+        return 
     }
     const imc = getIMC(peso, altura)
-    const nivelIMC = getNivelIMC()
+    const nivelIMC = getNivelIMC(imc)
 
-    console.log(imc, nivelIMC)
+    setResultado(`Seu IMC: ${imc} (${nivelIMC})`, true)
 
 }) 
 
+//calcula a faixa do IMC
 function getNivelIMC(imc){
     const nivel = ['Abaixo do Peso','Peso Normal', 'Sobrepeso', 'Obesidade Grau 1', 'Obesidade Grau 2', 'Obesidade Grau 3']
-    if (imc <= 18.5){
-        return nivel[0]
-    }
-    if (imc > 18.5 && imc <= 24.9){
-        return nivel[1]
-    }
-    if (imc >= 25 && imc <= 29.9){
-        return nivel[2]
-    }
-    if (imc >=30 && imc <= 34.9){
-        return nivel[3]
-    }
-    if (imc >= 35 && imc <=39.9) {
-        return nivel[4]
-    }
-    if (imc >= 40){
-        return nivel[5]
-    }
+    if (imc <= 18.5) return nivel[0]
+
+    if (imc > 18.5 && imc <= 24.9) return nivel[1]
+
+    if (imc >= 25 && imc <= 29.9) return nivel[2]
+
+    if (imc >=30 && imc <= 34.9) return nivel[3]
+
+    if (imc >= 35 && imc <=39.9) return nivel[4]
+
+    if (imc >= 40) return nivel[5]
 }
 
+//calcula o imc
 function getIMC(peso, altura){
     const soma = peso/ altura **2 
     return soma.toFixed(2)
 }
 
+//cria um paragrafo html
 function criaP(){
     const p = document.createElement('p')
     return p
 }
 
+// escreve no paragrafo criado 
 function setResultado(msg, isValid){
     const resultado = document.querySelector('.resultado')
     resultado.innerHTML =''
 
     const p = criaP(msg)
+    if (isValid){
+        p.classList.add('paragrafo-resultado')} 
+    else{
+        p.classList.add('paragrafo-resultado-erro')
+    }
     p.innerHTML = msg
     resultado.appendChild(p)
 }
