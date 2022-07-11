8d3c912f37a3e2a00be97b0861e54306c191430b
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-11 17:29:02 -0300

    For in

---
+++ FirstSteps/EstruturasDeRepetição/ForIn.js
@@ -0,0 +1,6 @@
+let pessoas = ['Pedro', 'Pablo', 'Lucélia', 'Felipe', 'Helen']
+let contador = 0
+for (contador in pessoas) {
+    console.log(`${contador} = ${pessoas[contador]}`)
+    contador++
+}
