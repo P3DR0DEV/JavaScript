0f7938e79f72fc1439ee8e5483f1760c13e4407a
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-12 10:27:51 -0300

    Conclusão EX

--- FirstSteps/EstruturasDeRepetição/Model/assets/js/main.js
+++ FirstSteps/EstruturasDeRepetição/Model/assets/js/main.js
@@ -1,15 +1,23 @@
 const elementos = [
+    {tag: 'h1', texto: 'Exercícios com Repetição'},
     {tag: 'p', texto: 'Frase 1'},
     {tag: 'div', texto: 'Frase 2'},
     {tag: 'footer', texto: 'Frase 3'},
     {tag: 'section', texto: 'Frase 4'}
 ]
 
+
+const container = document.querySelector('.container')
+const div = document.createElement('div')
+
 for (c in elementos){
-    const { tag } = elementos[c]
-    criaElemento(tag)
+    const { tag, texto } = elementos[c]
+    const tagCriada = document.createElement(tag)
+    const textoCriado = document.createTextNode(texto)
+
+    tagCriada.appendChild(textoCriado)
+    div.appendChild(tagCriada)
 }
-function criaElemento(tag){
-    const novoElemento = document.querySelector('.container')
-    novoElemento.createElement(`${tag}`)
-}
+
+
+container.appendChild(div)
