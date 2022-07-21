28a324cdacc37c8d512f5657ba7995c5549cfc63
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-21 16:57:54 -0300

    Node Crash Course

--- Node/server/server.js
+++ Node/server/server.js
@@ -1,6 +1,16 @@
 const http = require('http')
 const fs = require('fs')
 
-const server = http.createServer((request, response) => {
-    
+const server = http.createServer((req, res) => { // cria o servidor
+    console.log(req.url, req.method)
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/plain')
+    res.write('Hello World')
+    res.end()
+});
+
+server.listen(3000,'localhost', ()=>{ //recebe requests 
+    console.log('Listening for request on port 3000.')
 })
+
