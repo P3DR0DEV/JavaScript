79a373556e642a20c2fcaf4b0fc2755716cad9b9
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-22 16:46:04 -0300

    First use Express

---
+++ Node/server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+//express app
+const app = express();
+
+//listen for request
+
+app.listen(3000); // port 
+
+app.get('/'||'/index', (req,res)=>{
+    res.sendFile('./views/index.html',{
+        root:__dirname
+    });
+});
+
+app.get('/about', (req,res)=>{
+    res.sendFile('./views/about.html', {
+        root: __dirname
+    });
+});
+
+//redirectiong a page
+app.get('/about-us',(req,res)=>{
+    res.redirect('/about');
+});
+
+//404 page, tem que se o ultimo codigo da pagina
+
+app.use((req, res)=>{
+    res.sendFile('./views/404.html', {
+        root: __dirname
+    });
+})
