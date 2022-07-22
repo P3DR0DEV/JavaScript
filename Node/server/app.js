7a7d6ccbf368f01e9aad978e598c13043df0fed0
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-22 16:55:56 -0300

    add status 404

--- Node/server/app.js
+++ Node/server/app.js
@@ -26,7 +26,7 @@ app.get('/about-us',(req,res)=>{
 //404 page, tem que se o ultimo codigo da pagina
 
 app.use((req, res)=>{
-    res.sendFile('./views/404.html', {
+    res.status(404).sendFile('./views/404.html', {
         root: __dirname
     });
 })
