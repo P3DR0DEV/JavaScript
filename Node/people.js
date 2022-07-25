b03fd6a0754135ed34a1ef764f316c45e3d965e1
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-25 12:27:58 -0300

    add folder

--- Node/people.js
+++
@@ -1,7 +0,0 @@
-const pessoa = ['Pedro', 'Pablo', 'Nathalia', 'Maria Eduarda']
-const idade = [20, 34, 20, 19]
-
-module.exports ={
-    pessoa,
-    idade
-}
--- Node/tut01.js
+++
@@ -1,3 +0,0 @@
-console.log(__dirname) // caminho do arquivo selecionado
-console.log(__filename) // caminho completo com o arquivo incluso 
-
