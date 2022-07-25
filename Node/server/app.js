ee1e47b60cf8f8bf72dc296e26588f196d0a9b0d
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-07-25 12:12:41 -0300

    css add

--- Node/server/app.js
+++ Node/server/app.js
@@ -1,19 +1,22 @@
 const express = require('express');
-const path = require('path')
-const ejs = require('ejs')
+const path = require('path');
+const morgan = require('morgan');
 const app = express();
 
 
 // register view engines
+app.use(express.static(path.resolve(__dirname,'./public')))
 app.set('view engine', 'ejs');
-app.set('views', path.resolve(__dirname,'./views'))
+app.set('views', path.resolve(__dirname,'./views'));
+
+app.use(morgan('dev'))
 
 app.get('/', (req,res)=>{
     const blogs = [
         {title: 'Renan é gay', snippet: 'Renan é bem gay mesmo'},
         {title: 'Ancient é N', snippet: 'Ancient é bem N mesmo'},
         {title: 'Capivara é psico', snippet: 'Capivara fez uma mulher terminar e depois fez ela se matar'}
-    ]
+    ];
     return res.render('index', {
         title: 'Home',
         blogs
@@ -23,7 +26,7 @@ app.get('/', (req,res)=>{
 app.get('/about', (req,res)=>{
     return res.render('about', {
         title: 'About'
-    })
+    });
 });
 
 //redirectiong a page
@@ -34,16 +37,16 @@ app.get('/about-us',(req,res)=>{
 app.get('/blogs/create', (req,res)=>{
     return res.render('create', {
         title:'Create New Blog'
-    })
+    });
 });
-
 //!404 page, tem que se o ultimo codigo da pagina
 
 app.use((req, res)=>{
     res.status(404).render('404', {
         title: '404'
-    })
-})
+    });
+});
+
 
 //listen for request
 
