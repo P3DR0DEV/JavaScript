a94c98038c32bdea0e939538f2e2eb5d1ca9fcca
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-08-02 11:38:16 -0300

    Getter e setter

--- FirstSteps/objetos/objAdv/GettersSetters.js
+++ FirstSteps/objetos/objAdv/GettersSetters.js
@@ -1,23 +1,45 @@
+function Produto(nome, preço, estoque){
+    this.nome = nome
+    this.preço = preço
 
-function Produto(nome, estoque, preço){
     let estoquePrivado = estoque
     Object.defineProperty(this, 'estoque' , {
         enumerable: true,
         configurable: true,
-        set: function(valor){
-            if (typeof valor === 'number'){
-                estoquePrivado = valor;
-            }
-            
-            console.log('Numero inválido');
-            return;
+        get: function(){
+            return estoquePrivado
         },
-        get: function() {
-            console.log(estoquePrivado)
-        }
+        set: function(valor){
+            if (typeof valor !== 'number'){
+                console.log('Número inválido.')
+                return 
+            }
+            estoquePrivado = valor
+        },
     });
 };
 
+function criaProduto(nome){
+    let nomeValido = nome
+    return{
+        get nome(){
+            return nomeValido
+        },
+        set nome(nome){
+            if (typeof nome !== 'string'){
+                console.log('Nome inválido')
+                return 
+            };
+            return nomeValido 
+        }
+    } 
+}
 
-const Camisas = new Produto('Camisas', 'sakoifhsauidjsa', 20)
-console.log(Camisas.estoque)
+
+const camisas = new Produto('Camisas',50, 20)
+camisas.estoque = 30
+console.log(camisas.estoque)
+
+
+const camisas2 = criaProduto('Camisetas')
+console.log(camisas2.nome)
