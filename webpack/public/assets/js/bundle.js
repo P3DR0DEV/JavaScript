23918e020d65ff7e4d3326e513f515a8ef49c4fd
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-08-22 11:38:29 -0300

    Add webpack config

---
+++ webpack/public/assets/js/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+var nome = 'Pedro';
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
