6bf33f5ce88b69db066b77dfaa0ce9f37bdceaff
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-09-12 22:15:26 -0300

    add new dependencies

--- Tests/node/server.js
+++ Tests/node/server.js
@@ -3,20 +3,39 @@ const app = express();
 const mongoose = require('mongoose');
 const routes = require('./routers/routes');
 const path = require('path');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+
 
 require('dotenv').config();
 const mongodb = process.env.MONGODB_URI;
 
-app.use(routes);
-app.use(express.static(path.resolve(__dirname, 'public')))
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.set('views', path.resolve(__dirname, 'src' , 'views'))
-app.set('view engine', 'ejs')
+const sessionOptions = session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    },
+    store: MongoStore.create({ mongoUrl: mongodb })
+});
+app.use(sessionOptions);
+app.use(flash());
+
+app.set('views', path.resolve(__dirname, 'src' , 'views'));
+app.set('view engine', 'ejs');
+
+app.use(routes);
+
 
 mongoose.connect(mongodb)
- .then(app.listen(3000, ()=>{
+.then(app.listen(3000, ()=>{
     console.log('Running on 3000')
 }))
- .catch(e =>{
+.catch(e =>{
     console.log(e)
 })
--- Tests/node/src/controllers/home.controller.js
+++ Tests/node/src/controllers/home.controller.js
@@ -1,5 +1,5 @@
 const viewHome = (req, res) =>{
-    return res.render('home')
+    return res.render('home',{ title: 'Home Page' })
 }
 const viewAbout = (req, res) =>{
     return res.send('About Page')
