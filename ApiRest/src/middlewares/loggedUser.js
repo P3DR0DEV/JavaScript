c6e3334cf04f1d2feb56efd115d2beaee87d220c
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-10-04 12:56:29 -0300

    jwt middleware

--- ApiRest/src/middlewares/loggedUser.js
+++ ApiRest/src/middlewares/loggedUser.js
@@ -1,3 +1,28 @@
+import dotenv from 'dotenv';
+import { verify } from 'jsonwebtoken';
+
+dotenv.config();
+
 export default (req, res, next) => {
-  next();
+  const { authorization } = req.headers;
+
+  if (!authorization) return res.status(401).json({ errors: ['Required login'] });
+
+  const [texto, token] = authorization.split(' ');
+
+  try {
+    if (texto !== 'Bearer') return res.status(403).json({ errors: ['Invalid type token.'] });
+
+    const payload = verify(token, process.env.JWT_SECRET);
+
+    const { id, email } = payload;
+    req.userId = id;
+    req.userEmail = email;
+
+    return next();
+  } catch (e) {
+    return res.status(401).json({
+      errors: ['Token invalido ou expirado'],
+    });
+  }
 };
--- ApiRest/src/routes/userRoutes.js
+++ ApiRest/src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import loggedUser from '../middlewares/loggedUser';
 
 const router = new Router();
 
@@ -7,7 +8,7 @@ router.get('/', userController.index);
 router.get('/:id', userController.show);
 
 router.post('/', userController.create);
-router.delete('/:id', userController.delete);
-router.put('/:id', userController.update);
+router.delete('/', loggedUser, userController.delete);
+router.put('/', loggedUser, userController.update);
 
 export default router;
