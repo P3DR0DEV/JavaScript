b4a73bddec405555c44343170d42f5c71bf68168
Pedro Henrique Campos Mendes <pedrohenriquecamposmendes@hotmail.com>
2022-10-25 12:51:18 -0300

    update file uploads with multer

--- ApiRest/src/controllers/UploadController.js
+++ ApiRest/src/controllers/UploadController.js
@@ -1,6 +1,16 @@
+import multer from 'multer';
+import multerCFG from '../config/multerCFG';
+
+const uploads = multer(multerCFG).single('profilePic');
+
 class UploadController {
   create(req, res) {
-    res.json(req.file);
+    return uploads(req, res, (err) => {
+      if (err) {
+        return res.status(400).json({ errors: [err.code] });
+      }
+      return res.json(req.file);
+    });
   }
 }
 
--- ApiRest/src/routes/uploadRoutes.js
+++ ApiRest/src/routes/uploadRoutes.js
@@ -1,12 +1,9 @@
-import multer from 'multer';
 import { Router } from 'express';
-import UploadController from '../controllers/UploadController';
-import multerCFG from '../config/multerCFG';
 
-const uploads = multer(multerCFG);
+import UploadController from '../controllers/UploadController';
 
 const router = new Router();
 
-router.post('/', uploads.single('profilePic'), UploadController.create);
+router.post('/', UploadController.create);
 
 export default router;
